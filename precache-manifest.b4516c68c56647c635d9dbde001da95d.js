f4761a4a8411976b6fd0b81821dd3ce75923eb08
Jill Moore <okjill@gmail.com>
2020-06-04 22:33:43 -0500

    Updates

---
+++ precache-manifest.b4516c68c56647c635d9dbde001da95d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4468a327bcc7d75398198d8f0ee33665",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "5b0f12dd115b01c27396",
+    "url": "/memory/static/css/main.1eeee309.chunk.css"
+  },
+  {
+    "revision": "69bc7651f84f4e9069e3",
+    "url": "/memory/static/js/2.479bf021.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory/static/js/2.479bf021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b0f12dd115b01c27396",
+    "url": "/memory/static/js/main.eeb2b830.chunk.js"
+  },
+  {
+    "revision": "7ed0e48e27aa87b00c38",
+    "url": "/memory/static/js/runtime-main.31103d8b.js"
+  },
+  {
+    "revision": "b3c4a248918c27c52c455d86ab29561b",
+    "url": "/memory/static/media/colors.b3c4a248.png"
+  },
+  {
+    "revision": "babea8b655f6c7b1b97df4894f53a159",
+    "url": "/memory/static/media/memory.babea8b6.png"
+  },
+  {
+    "revision": "9605271602dbc71304081c35358ec3fc",
+    "url": "/memory/static/media/numbers.96052716.png"
+  },
+  {
+    "revision": "9b9dce84f1d8664916888de047ce2a9f",
+    "url": "/memory/static/media/zodiac.9b9dce84.png"
+  }
+]);
